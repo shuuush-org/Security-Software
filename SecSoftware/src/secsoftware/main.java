6e8b3cdd1daf904f3fbcaf734951033b250f7e83
Thomas Little <telittlegh@gmail.com>
2022-09-02 16:14:48 -0400

    Started!

--- SecSoftware/src/secsoftware/main.java
+++ SecSoftware/src/secsoftware/main.java
@@ -1,20 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package secsoftware;
 
 /**
- *
  * @author Tommy
  */
 public class main {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Instance GUI
+        mainFrame GUI = new mainFrame();
+        GUI.setVisible(true);
     }
     
 }

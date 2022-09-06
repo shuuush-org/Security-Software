22bce75cb9d0ef040b5f6e5025e59b899421f281
Thomas Little <telittlegh@gmail.com>
2022-09-05 21:12:17 -0300

    fix

---
+++ Security Software/src/securitySoftware/MainFrame.java
@@ -0,0 +1,103 @@
+package securitySoftware;
+
+import com.formdev.flatlaf.FlatLightLaf;
+
+/**
+ * @author Tommy
+ */
+public class MainFrame extends javax.swing.JFrame {
+
+    public MainFrame() {
+        FlatLightLaf.setup();
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        panContent = new javax.swing.JPanel();
+        panBotones = new javax.swing.JPanel();
+        btnPasswordManager = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Security Software");
+        setName("fraMain"); // NOI18N
+        setResizable(false);
+
+        panContent.setPreferredSize(new java.awt.Dimension(1000, 720));
+
+        javax.swing.GroupLayout panContentLayout = new javax.swing.GroupLayout(panContent);
+        panContent.setLayout(panContentLayout);
+        panContentLayout.setHorizontalGroup(
+            panContentLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1000, Short.MAX_VALUE)
+        );
+        panContentLayout.setVerticalGroup(
+            panContentLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 720, Short.MAX_VALUE)
+        );
+
+        btnPasswordManager.setText("PASSWORD MANAGER");
+        btnPasswordManager.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnPasswordManagerActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout panBotonesLayout = new javax.swing.GroupLayout(panBotones);
+        panBotones.setLayout(panBotonesLayout);
+        panBotonesLayout.setHorizontalGroup(
+            panBotonesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panBotonesLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(btnPasswordManager, javax.swing.GroupLayout.DEFAULT_SIZE, 262, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        panBotonesLayout.setVerticalGroup(
+            panBotonesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panBotonesLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(btnPasswordManager, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addComponent(panBotones, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(panContent, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(panBotones, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(panContent, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnPasswordManagerActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPasswordManagerActionPerformed
+        // Instancing Password class
+        FraPassword fpwd = new FraPassword();
+
+        // Pane content is cleared
+        panContent.removeAll();
+
+        // Pane content is replaced by Password class's content
+        panContent.add(fpwd.getContentPane());
+
+        // Pane is updated
+        panContent.repaint();
+        panContent.revalidate();
+    }//GEN-LAST:event_btnPasswordManagerActionPerformed
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnPasswordManager;
+    private javax.swing.JPanel panBotones;
+    private javax.swing.JPanel panContent;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ Security Software/src/securitySoftware/main.java
@@ -0,0 +1,16 @@
+package securitySoftware;
+
+/**
+ * @author Tommy
+ */
+public class main {
+    public static void main(String[] args) {
+        // Check if software needs update
+        
+        
+        // Instance and diplay program
+        MainFrame GUI = new MainFrame();
+        GUI.setVisible(true);
+    }
+    
+}
